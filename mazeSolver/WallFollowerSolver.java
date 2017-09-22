4d191def17ba0e744c3dc27630f0e7734e5bbb9e
Craig Barry <craig_barry_123@hotmail.com>
2017-09-22 22:16:44 +1000

    Simplified Turn in Primms Algorithm

--- mazeSolver/WallFollowerSolver.java
+++ mazeSolver/WallFollowerSolver.java
@@ -14,6 +14,9 @@ public class WallFollowerSolver implements MazeSolver {
 	private int travelDir = 0;
 	private Cell cCell = null;
 	private boolean isSolved = false;
+
+	private final int LEFT = 1;
+	private final int RIGHT = -1;
 	
 	@Override
 	public void solveMaze(Maze maze) {
@@ -32,13 +35,13 @@ public class WallFollowerSolver implements MazeSolver {
 
 			if(canMoveLeft(maze, cCell, travelDir)) {
 				System.out.println("Turned Left");
-				travelDir = turnLeft(maze, travelDir);
+				travelDir = turn(maze, travelDir, LEFT);
 			}else if(canMoveStraight(cCell, travelDir)) {
 				System.out.println("Went Straight");
 				//Do nothing
 			}else if(canMoveRight(maze, cCell, travelDir)) {
 				System.out.println("Turned Right");
-				travelDir = turnRight(maze, travelDir);
+				travelDir = turn(maze, travelDir, RIGHT);
 			}else {
 				travelDir = turnAround(travelDir);
 				System.out.println("Turned Around");
@@ -58,39 +61,68 @@ public class WallFollowerSolver implements MazeSolver {
 
 	} // end of solveMaze()
 
-	private int turnLeft(Maze m, int cDir) {
-		//Increment the direction
-		cDir += 1;
+	// private int turnLeft(Maze m, int cDir) {
+	// 	//Increment the direction
+	// 	cDir += 1;
+	// 	//If the direction is at the max
+	// 	if(cDir == Maze.NUM_DIR)
+	// 		//Return the first direction
+	// 		cDir = 0;
+
+	// 	//Northeast doesn't exist in non-hexagonal maze, use next index
+	// 	if(m.type != Maze.HEX && cDir == Maze.NORTHEAST)
+	// 		cDir += 1;
+
+	// 	//Southeast doesn't exist in non-hexagonal maze, use next index
+	// 	if(m.type != Maze.HEX && cDir == Maze.SOUTHWEST)
+	// 		cDir += 1;
+
+	// 	//Return the direction after turing left
+	// 	return cDir;
+	// }
+
+	// private int turnRight(Maze m, int cDir) {
+	// 	//Decrement the direction
+	// 	cDir -= 1;
+	// 	//If the direction is invalid
+	// 	if(cDir == -1)
+	// 		//Return the correct direction
+	// 		cDir = Maze.NUM_DIR - 1;
+
+	// 	//Northeast doesn't exist in non-hexagonal maze, use next index
+	// 	if(m.type == Maze.NORMAL && cDir == Maze.NORTHEAST)
+	// 		cDir -= 1;
+
+	// 	//Southeast doesn't exist in non-hexagonal maze, use next index
+	// 	if(m.type == Maze.NORMAL && cDir == Maze.SOUTHWEST)
+	// 		cDir -= 1;
+
+	// 	//Return the direction after turning right
+	// 	return cDir;
+	// }
+
+	private int turn(Maze m, int cDir, int turnDir) {
+		//turn in the given direction
+		cDir += turnDir;
 		//If the direction is at the max
-		if(cDir == Maze.NUM_DIR)
+		if(cDir >= Maze.NUM_DIR)
 			//Return the first direction
 			cDir = 0;
 
-		if(m.type == Maze.NORMAL && cDir == Maze.NORTHEAST)
-			cDir += 1;
-
-		if(m.type == Maze.NORMAL && cDir == Maze.SOUTHWEST)
-			cDir += 1;
-
-		//Return the direction after turing left
-		return cDir;
-	}
-
-	private int turnRight(Maze m, int cDir) {
-		//Decrement the direction
-		cDir -= 1;
-		//If the direction is invalid
-		if(cDir == -1)
-			//Return the correct direction
+		//If the direction is below the min
+		if(cDir < 0)
+			//Return the last direction
 			cDir = Maze.NUM_DIR - 1;
 
-		if(m.type == Maze.NORMAL && cDir == Maze.NORTHEAST)
-			cDir -= 1;
+		//Northeast doesn't exist in non-hexagonal maze, use next index
+		if(m.type != Maze.HEX && cDir == Maze.NORTHEAST)
+			cDir += turnDir;
 
-		if(m.type == Maze.NORMAL && cDir == Maze.SOUTHWEST)
-			cDir -= 1;
+		//Southeast doesn't exist in non-hexagonal maze, use next index
+		if(m.type != Maze.HEX && cDir == Maze.SOUTHWEST)
+			cDir += turnDir;
 
-		//Return the direction after turning right
+		//Return the direction after turing left
 		return cDir;
 	}
 
@@ -111,12 +143,12 @@ public class WallFollowerSolver implements MazeSolver {
 	}
 
 	private boolean canMoveLeft(Maze m, Cell c, int dir) {
-		dir = turnLeft(m, dir);
+		dir = turn(m, dir, LEFT);
 		return !c.wall[dir].present && c.neigh[dir] != null;
 	}
 
 	private boolean canMoveRight(Maze m, Cell c, int dir) {
-		dir = turnRight(m, dir);
+		dir = turn(m, dir, RIGHT);
 		return !c.wall[dir].present && c.neigh[dir] != null;
 	}
     
