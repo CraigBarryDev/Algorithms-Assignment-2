65d70545608413a13db0d806cdc292fe564a6da8
Craig Barry <craig_barry_123@hotmail.com>
2017-09-22 22:27:31 +1000

    Solved Hex Maze W/  Wall Follower

--- mazeSolver/WallFollowerSolver.java
+++ mazeSolver/WallFollowerSolver.java
@@ -33,19 +33,22 @@ public class WallFollowerSolver implements MazeSolver {
 		while(cCell != maze.exit) {
 			//While the traveling direction can't go straight
 
-			if(canMoveLeft(maze, cCell, travelDir)) {
-				System.out.println("Turned Left");
-				travelDir = turn(maze, travelDir, LEFT);
-			}else if(canMoveStraight(cCell, travelDir)) {
-				System.out.println("Went Straight");
-				//Do nothing
-			}else if(canMoveRight(maze, cCell, travelDir)) {
-				System.out.println("Turned Right");
-				travelDir = turn(maze, travelDir, RIGHT);
-			}else {
-				travelDir = turnAround(travelDir);
-				System.out.println("Turned Around");
-			}
+			// if(canMoveLeft(maze, cCell, travelDir)) {
+			// 	System.out.println("Turned Left");
+			// 	travelDir = turn(maze, travelDir, LEFT);
+			// }else if(canMoveStraight(cCell, travelDir)) {
+			// 	System.out.println("Went Straight");
+			// 	//Do nothing
+			// }else if(canMoveRight(maze, cCell, travelDir)) {
+			// 	System.out.println("Turned Right");
+			// 	travelDir = turn(maze, travelDir, RIGHT);
+			// }else {
+			// 	travelDir = turnAround(travelDir);
+			// 	System.out.println("Turned Around");
+			// }
+
+			//Get the next direction to travel
+			travelDir = getNextTravelDirection(maze, cCell, travelDir);
 
 			//Now that you can move straight, move straight
 			cCell = travelStraight(maze, cCell, travelDir);
@@ -60,46 +63,6 @@ public class WallFollowerSolver implements MazeSolver {
 		isSolved = true;
 
 	} // end of solveMaze()
-
-	// private int turnLeft(Maze m, int cDir) {
-	// 	//Increment the direction
-	// 	cDir += 1;
-	// 	//If the direction is at the max
-	// 	if(cDir == Maze.NUM_DIR)
-	// 		//Return the first direction
-	// 		cDir = 0;
-
-	// 	//Northeast doesn't exist in non-hexagonal maze, use next index
-	// 	if(m.type != Maze.HEX && cDir == Maze.NORTHEAST)
-	// 		cDir += 1;
-
-	// 	//Southeast doesn't exist in non-hexagonal maze, use next index
-	// 	if(m.type != Maze.HEX && cDir == Maze.SOUTHWEST)
-	// 		cDir += 1;
-
-	// 	//Return the direction after turing left
-	// 	return cDir;
-	// }
-
-	// private int turnRight(Maze m, int cDir) {
-	// 	//Decrement the direction
-	// 	cDir -= 1;
-	// 	//If the direction is invalid
-	// 	if(cDir == -1)
-	// 		//Return the correct direction
-	// 		cDir = Maze.NUM_DIR - 1;
-
-	// 	//Northeast doesn't exist in non-hexagonal maze, use next index
-	// 	if(m.type == Maze.NORMAL && cDir == Maze.NORTHEAST)
-	// 		cDir -= 1;
-
-	// 	//Southeast doesn't exist in non-hexagonal maze, use next index
-	// 	if(m.type == Maze.NORMAL && cDir == Maze.SOUTHWEST)
-	// 		cDir -= 1;
-
-	// 	//Return the direction after turning right
-	// 	return cDir;
-	// }
 
 	private int turn(Maze m, int cDir, int turnDir) {
 		//turn in the given direction
@@ -150,6 +113,20 @@ public class WallFollowerSolver implements MazeSolver {
 	private boolean canMoveRight(Maze m, Cell c, int dir) {
 		dir = turn(m, dir, RIGHT);
 		return !c.wall[dir].present && c.neigh[dir] != null;
+	}
+
+	private int getNextTravelDirection(Maze m, Cell c, int dir) {
+		//Reverse the travel direction and turn right
+		//This will give start the direction of the leftmost direction
+		dir = turn(m, Maze.oppoDir[dir], RIGHT);
+
+		//Iterate through all the directions by turning right
+		//if we can travel this direction then it is the next travel direction
+		while(!canMoveStraight(c, dir))
+			dir = turn(m, dir, RIGHT);
+
+		//Return the travel direction
+		return dir;
 	}
     
     
